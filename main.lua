c03e7e1a52ca1a0373b7fcc0c210a21de9c22b33
Maxwell317898 <101601071+Maxwell317898@users.noreply.github.com>
2024-08-17 16:12:56 -0700

    sfdjjasdkjkdsfgbadighdsf
    fhadhghaehu cuir

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 local username = "admin"
 local password = "admin"
 
+local apps = {
+    "ping"
+}
 
 print("enter ur password for ".. username)
 local input = io.read()
@@ -9,3 +12,32 @@ if input == password then
 elseif input ~= password then
     print("You are a STUPID thing. You mean NOTHING. You should kill yourself NOW.")
 end
+
+function ping()
+    print("poong")
+    readtorunapp()
+end
+
+function readtorunapp()
+    io.write(">")
+    input = io.read()
+    if input == "ping" then
+        ping()
+    else
+        print("you are not typing like a human try again")
+        readtorunapp()
+    end
+end
+
+
+
+
+
+
+os.execute("cls")
+print("listing your apps and such")
+for count = 1, 1 do
+    print(apps[1])
+end
+print("to runn and app just type its name")
+readtorunapp()
