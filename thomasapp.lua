75ac745ae6081f9e9e4695866d158de4360812f3
something-i-guess <140275565+something-i-guess@users.noreply.github.com>
2024-08-17 16:48:40 +0100

    Update thomasapp.lua

--- thomasapp.lua
+++ thomasapp.lua
@@ -6,7 +6,7 @@ end
 
 function thomasapp.main()
     for i = 1, 100 do
-        local baba = math.random(0.000,99.999)
+        local baba = math.random(0,100)
         print(baba)
         sleep(0.01)
     end
