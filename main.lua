624dc46506c0570eea7ef3e3071525bbd817e08d
Maxwell317898 <101601071+Maxwell317898@users.noreply.github.com>
2024-08-17 18:40:23 -0700

    backup before chatgpt take other the world

--- main.lua
+++ main.lua
@@ -2,9 +2,31 @@ local username = "admin"
 local password = "admin"
 local maxapp = require("maxapps")
 local thomasapp = require("thomasapp")
+local sudomode = false
 
-local apps = {
+
+
+
+print("enter ur password for ".. username) -- auth
+local input = io.read()
+if input == password then
+    print("You are human")
+elseif input ~= password then
+    print("You are a STUPID thing. You mean NOTHING. You should kill yourself NOW.")
+end
+
+function ping() -- sudo app function 
+    print("poong")
+    readtorunapp()
+end
+
+local sudoapps = { -- sudoapps sudo cmdline needed
     "ping",
+}
+local sudofuncs = {
+    ping,
+}
+local apps = { -- user apps
     "list",
     "thomasapp",
     "thomasmore",
@@ -14,57 +36,43 @@ local apps = {
     "binerygen",
 }
 
-print("enter ur password for ".. username)
-local input = io.read()
-if input == password then
-    print("You are human")
-elseif input ~= password then
-    print("You are a STUPID thing. You mean NOTHING. You should kill yourself NOW.")
-end
-
-function ping()
-    print("poong")
+function readtorunapp() -- read for an app and run
+    io.write(">")
+    local input = io.read()
+    if input == "sudo" then
+        if sudomode == false then
+            sudomode = true
+        else
+            sudomode = false
+        end
+        readtorunapp()
+    end
+    if sudomode == false then
+        for index, value in ipairs(apps) do
+            if value == input then
+                apps.index()
+            end
+        end
+    else
+        for i, v in ipairs(sudoapps) do
+            if v == input then
+                sudofuncs.i()
+            end
+        end
+    end
+    print("write like a human")
     readtorunapp()
 end
 
-function readtorunapp()
-    io.write(">")
-    input = io.read()
-    if input == "ping" then
-        ping()
-    elseif input == "list" then
-        listapps()
-    elseif input == "thomasapp" then
-        thomasapp.main()
-    elseif input == "thomasmore" then
-        thomasapp.evenmore()
-    elseif input == "yes" then
-        thomasapp.yes()
-    elseif input == "no" then
-        thomasapp.no()
-    elseif input == "thomastest" then
-        thomasapp.test()
-    elseif input == "binerygen" then
-        maxapp.randombin()
-    else
-        os.execute("cls")
-        print("you are not typing like a human try again")
-        readtorunapp()
-    end
-end
-
-function listapps()
+function listapps() -- list app function played on start also called with list
     os.execute("cls")
     print("listing your apps and such")
-    for count = 1, 8 do
+    for count = 1, 6 do
         print(apps[count])
     end
     print("to runn and app just type its name")
     readtorunapp()
 end
-
-
-
 
 
 listapps() -- defult intro to apps
