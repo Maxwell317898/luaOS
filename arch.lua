3e17c00e4954517ec985529e4384f828e01e30cc
Maxwell317898 <101601071+Maxwell317898@users.noreply.github.com>
2024-08-29 07:51:10 -0700

    update 1 of 2-5

--- arch.lua
+++ arch.lua
@@ -1,101 +1,98 @@
-local username = "admin"
-local password = "admin"
-local maxapp = require("maxapps")
-local thomasapp = require("thomasapp")
-local sudomode = false
+local usrnm = "admin" -- put in username. defult "admin"
+local pass = "admin" -- put in password. defult "admin"
 
-print("Enter your password for " .. username) -- auth
-local input = io.read()
-if input == password then
-    print("You are human")
-else
-    print("You are a STUPID thing. You mean NOTHING. You should kill yourself NOW.")
-    os.exit(1)
-end
-
-function ping() -- sudo app function 
-    print("poong")
-end
+local authed = false
+local ram = {}
 
 
-local sudoapps = { -- sudoapps sudo cmdline needed
+local cmdnam = {
     "list",
-    "ping",
-    "sasti",
-    "sifr",
-
-}
-local sudofuncs = {
-    listapps,
-    ping,
-    maxapp.sudoappsuptestinteractive,
-    maxapp.sudoreadfileinteractive,
+    "reg",
 }
 
-local apps = { -- user apps
-    "list",
-    "thomasapp",
-    "thomasmore",
-    "yesandno",
-    "gibberish",
-    "binarygen",
+-- ANSI escape codes for colors
+local colors = {
+    reset = "\27[0m",
+    red = "\27[31m",
+    green = "\27[32m",
+    yellow = "\27[33m",
+    blue = "\27[34m",
+    magenta = "\27[35m",
+    cyan = "\27[36m",
+    white = "\27[37m",
+    black_bg = "\27[40m",
+    red_bg = "\27[41m",
+    green_bg = "\27[42m",
+    yellow_bg = "\27[43m",
+    blue_bg = "\27[44m",
+    magenta_bg = "\27[45m",
+    cyan_bg = "\27[46m",
+    white_bg = "\27[47m",
 }
 
-function listapps() -- list app function played on start also called with list
-    os.execute("cls") -- or os.execute("clear") if on UNIX systems
-    print("Listing your apps and such:")
-    if sudomode == false then
-        for _, app in ipairs(apps) do
-            print(app)
-        end
-    else
-        for _, sapp in ipairs(sudoapps) do
-            print(sapp)
-        end
+function list()
+    os.execute("cls")
+    print("here are all commands:")
+    for _,cmdname in ipairs(cmdnam) do
+        print(cmdname)
     end
-    print("To run an app, just type its name")
+    readcommand()
 end
 
-local appfuncs = {
-    listapps,
-    thomasapp.main,
-    thomasapp.evenmore,
-    thomasapp.yesandno,
-    thomasapp.test,
-    maxapp.randombin,
-}
-
-
-
-function readtorunapp() -- read for an app and run
-    io.write(">")
-    local input = io.read()
-
-    if input == "sudo" then
-        sudomode = not sudomode
-        print("Sudo mode is now " .. (sudomode and "enabled" or "disabled"))
-        return readtorunapp()
+function reg() -- depracticating. to meny bugs that i cant fix
+    if ram.regreex ~= true then -- check not reexecute
+    os.execute("cls")
     end
-
-    if sudomode then
-        for index, value in ipairs(sudoapps) do
-            if value == input then
-                sudofuncs[index]()
-                return readtorunapp()
-            end
-        end
-    else
-        for index, value in ipairs(apps) do
-            if value == input then
-                appfuncs[index]()  -- Execute the corresponding app function
-                return readtorunapp()
-            end
-        end
+    print(colors.white_bg.. "You have entered Reg editor/reader to EXIT use / exit , for more help on how to use reg edit/read just run / reghelp".. colors.reset)
+    io.write("@/ ") ram.reginput = io.read()
+    if ram.reginput == "ramdump" then
+        print("running dump:")
+        for i, cont in ipairs(ram) do
+            print(i.. ", ".. cont)
+        end        
+        print(colors.green_bg.. "done, sending you to main command line".. colors.reset)
+    elseif ram.reginput == "help" then
+        print("BEFORE YOU USE REG EDIT/READ: reg editing can damage this system not your real one but this instance, you can also use this to changesome settings")
+        print("commands: regdump : lists the whole ram")
+        ram.regreex = true
+        reg()
+    elseif ram.reginput == "exit" then
+        print("sending you to main command line")
+        readcommand()
     end
-
-    print("Invalid input. Please try again.")
-    return readtorunapp()
 end
 
-listapps() -- default intro to apps
-readtorunapp()
+local cmdcom = {
+    list,
+    reg,
+}
+
+function readcommand()
+    print("enter a command here, to see commands just run  list")
+    io.write("> ") ram.cmdinput = io.read()
+    for i,cmd in ipairs(cmdnam) do
+        if ram.cmdinput == cmd then
+            cmdcom[i]()
+        end
+    end
+end
+
+function auth() -- password and login
+    print(colors.blue.. "authenticating".. colors.reset)
+    if ram.authretry ~= true then -- only clear when fisrt try so thay can see "incorrect password"
+    os.execute("cls") -- win only arch 3 only has support for windows
+    end
+    print(colors.white_bg.. "enter the password for: ".. usrnm.. colors.reset)
+    ram.passinput = io.read()
+    if ram.passinput == pass then
+        print("correct password sending you to desktop...")
+        authed = true
+        list()
+    else
+        print(colors.red_bg.. "incorrect password.. you will be sent back to login".. colors.reset)
+        ram.authretry = true
+        auth()
+    end
+end
+
+auth()
--- maxapps.lua
+++ maxapps.lua
@@ -1,99 +1,18 @@
 local max = {}--[[
-maxapp support can be used by other apps 
-made by maxwell317898
-apps using these the MAXAPP policy(s) must abide by a few rules
-list is on the github
-]]
-function max.randombin()
-    print("how long u want binary for NO LETTERS")
-    local inp = io.read()
-    for i = 1, inp do
-        io.write(math.random(0,1))
-    end
-    io.write("\n")
-    readtorunapp()
-end
+maxapp libs for use on applications using arch-line lua. Make sure your apps (if you want it to be inculded by defult) complie with the maxapp policys
+by maxwell3178]]
 
-function max.appsupport(cachdata) -- put data in the cachdata field and it should return its name
-    local folder = "maxappsfold/"
-    
-    local fileName = folder .. tostring(math.random(1, 999999)) .. ".txt"
-    
-    local file = io.open(fileName, "w")
-    
-    if file then
-        file:write(cachdata)
-        file:close()
-        
-        return fileName
+function codestore(data, vertoplaceID) -- places data in file. if vertoplaceID is nothing it will return the id needed to open it again use vertoplaceID to change a ver to the ID
+    local id = math.random(1, 99999999)
+    io.open("system/saves/".. id, "w")
+    io.write(data)
+    if vertoplaceID == nil then
+        return id
     else
-        print("Error: Could not open file.")
-        return nil
+        vertoplaceID = id -- should work with no error handeling....
     end
 end
 
-function max.appread(filnum)
-    local file = io.open(filnum, "r")
-    if file then
-        local data = file:read("*a") 
-        file:close()
-        return data
-    else
-        warn("maxapp: could not open file (check the file name)")
-        return false
-    end
-end
 
-function max.sudoappsuptestinteractive()
-    print("what data do you want to store no multi line stuff")
-    io.write("!~!>") local input = io.read()
-    if input ~= "" then
-        print("remember this file name it is your savefile maby even copy it   ".. max.appsupport(input))
-    end
-    print("type in the number (no .txt or maxappsfold/) of a savefile you want to read:")
-    io.write("! ") input = io.read()
-    local data = max.appread("maxappsfold/".. input.. ".txt")
-    if data then
-        print("here is the data:")
-        print(data)
-        print("sending you back into archline")
-    else
-        print("Error: Could not read from the file.")
-    end
-    readtorunapp()
-end
-
-local SFIRreapeat = false
-function max.sudoreadfileinteractive() 
-    if SFIRreapeat == false then
-        print("SIFR you have entered sudo interactve file reader mode (SIFR) to exit run ? for help run ??? to read a file in the cach pool type its number")
-    else
-        print("SIFR for help run ??? not ? (exit)")
-    end
-    io.write("!=!>") local input = tonumber(io.read())
-    if type(input) == "number" then
-        print("data:")
-        print(max.appread("maxappsfold/".. input.. ".txt"))
-        SFIRreapeat = true
-        max.sudoreadfileinteractive()
-    elseif input == nil then
-        print("SIFR command prompt for other commands: ? exit and ??? for help")
-        input = io.read()
-        if input == "?" then
-            readtorunapp()
-        elseif input == "???" then
-            print("SIFR Sudo Interactive File Reader part of the MAXAPP policy by maxwell317898 on github you will be sent back to the sifr to exit come back here and run ?")
-            sleep(1)
-            max.sudoreadfileinteractive()
-        else
-
-        end
-    else
-        os.execute("cls")
-        SFIRreapeat = true
-        print("SIFR only use numbers try again")
-        max.sudoreadfileinteractive()
-    end
-end
 
 return max
