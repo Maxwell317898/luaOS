fcee53ffe0aa3fccd92fb14f9efd0bae9998ccd0
something-i-guess <140275565+something-i-guess@users.noreply.github.com>
2024-10-31 10:36:49 +0000

    Update thomsapp.lua

--- thomsapp.lua
+++ thomsapp.lua
@@ -1,12 +1,13 @@
-print("what thing do you want:")
+print("what thing do you want from the list:")
+print("poopy")
 local read = io.read()
 
-function poopy()
+function binary()
     for i = 1, 100 do
-        print(math.random(1,100))
+        print(math.random(0,1))
     end
 end
 
 if read == "poopy" then
-    poopy()
+    binary()
 end
