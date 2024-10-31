26781239d97e39a0d6da2c71e4a0329002d3ce40
something-i-guess <140275565+something-i-guess@users.noreply.github.com>
2024-10-31 10:45:29 +0000

    Update thomsapp.lua

--- thomsapp.lua
+++ thomsapp.lua
@@ -1,9 +1,15 @@
 print("what thing do you want from the list:")
 print("binary")
 local read = io.read()
+function sleep(n)
+    if n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL") end
+end
 
 function binary()
-    for i = 1, 100 do
+    print("how much (you have 5 seconds to type)")
+    sleep(5)
+    local howmuch = io.read()
+    for i = 1, howmuch do
         io.write(math.random(0,1))
     end
     print(" here is ur binary")
